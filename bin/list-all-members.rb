6c5c7f90fe1dd4c1601503e40fb432c3944b5aa0
David Salgado <david@digitalronin.com>
2020-12-07 14:46:40 +0900

    Remove unneeded scripts

--- bin/list-all-members.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative "../lib/all_members"
-
-pp AllMembers::Organization.new(login: "ministryofjustice").members
--- bin/list-team-members.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative "../lib/all_members"
-
-pp AllMembers::Team.new(login: "ministryofjustice", name: "developers").members
